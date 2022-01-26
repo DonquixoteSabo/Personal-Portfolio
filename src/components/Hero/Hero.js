c39bb9b6bb33bd08d681e9b91990892d247c2a22
DonquixoteSabo <mixboksu@gmail.com>
2022-01-26 12:44:10 +0100

    feat: Add Hero section
    Still have to correct some mobile views

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookSquare, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 import './Hero.scss';
 
 const Hero = () => {
   return (
     <section className='hero'>
       <div className='hero__background'></div>
-      <div className='hero__display-text'></div>
+      <div className='hero__display-text'>
+        <div className='hero__text-container'>
+          <span>Hi, I am</span>
+          <span> Kamil Budzik</span>
+          <span>Front-end developer</span>
+        </div>
+        <div className='hero__socials'>
+          <FontAwesomeIcon icon={faGithubSquare} className='github' />
+          <FontAwesomeIcon icon={faFacebookSquare} className='facebook' />
+          <FontAwesomeIcon icon={faEnvelope} className='email' />
+        </div>
+      </div>
     </section>
   );
 };
