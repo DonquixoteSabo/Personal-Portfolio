86d637413e2bef482ead8cbd62e356591b2bd2f1
DonquixoteSabo <mixboksu@gmail.com>
2022-02-12 18:11:31 +0100

    refactor: Refactor Skills animations
    Replace GSAP with pure css
    Replace useEffect with media queries

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,59 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import gsap from 'gsap';
+import React from 'react';
 //content
 import { skillsData } from './data';
 //css
 import './Skills.scss';
 
 const Skills = () => {
-  const [width, setWidth] = useState(window.innerWidth);
-  const [skills, setSkills] = useState([]);
-  // const tl = useRef(null);
-
-  const BREAKPOINT = 700;
-
-  useEffect(() => {
-    window.addEventListener('resize', () => setWidth(window.innerWidth));
-  }, []);
-  useEffect(() => {
-    if (width > BREAKPOINT) {
-      setSkills(skillsData);
-    } else {
-      setSkills(skillsData.filter(({ smallDevice }) => smallDevice));
-    }
-  }, [width]);
-
-
-  const playAnimation = (index) => {
-    const el = document.getElementById(index);
-    // tl.current = gsap.timeline();
-    //
-    // tl.current
-    //   .to(el, { x: '-10px', duration: '1s' });
-    gsap.to(el, { x: '-10px', duration: '1s' });
-  };
-
-  const reverseAnimation = (index) => {
-    const el = document.getElementById(index);
-    // tl.current = gsap.timeline();
-    //
-    // tl.current
-    //   .to(el, { x: '0', duration: '1s' });
-    gsap.to(el, { x: 0, duration: '1s' });
-
-  };
-
   return (
     <section className='skills'>
       <header className='skills__title'>
         <h1>Skills</h1>
       </header>
       <ul className='skills__list'>
-        {skills.map(({ icon, name, smallDevice, text }, index) => (
-          <li id={index} key={name} className='skills__item' onMouseEnter={() => playAnimation(index)}
-              onMouseLeave={() => reverseAnimation(index)}>
-            <img className='skills__img' src={icon} alt={name} />
-            <ul className={`skills__inner-list`}>
+        {skillsData.map(({ icon, name, smallDevice, text }, index) => (
+          <li key={name} className={`skills__item ${!smallDevice && 'small-device'}`}>
+            <img className={`skills__img skills__img--${index}`} src={icon} alt={name} />
+            <ul className={`skills__inner-list skills__inner-list--${index}`}>
               {text.map((text) => (
                 <li key={text}>{text}</li>
               ))}
