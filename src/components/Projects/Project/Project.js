355c117c96ec073e6b35c769acdd0e9ff35911e2
DonquixoteSabo <mixboksu@gmail.com>
2022-03-09 17:51:47 +0100

    feat: Mock JSON data

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -6,12 +6,18 @@ import {faSearch} from '@fortawesome/free-solid-svg-icons';
 //styles
 import './Project.scss';
 
-const Project = ({title, description, link, img, live}) => {
+function createMarkup(description) {
+    return {__html: description};
+}
+//TODO
+// after you get images from your graphic designer refactor these styles because they don't look that good with these images.
+const Project = ({title, description, link, image, live, id}) => {
     return (
-        <li className='project' key={title}>
+        <li className='project' key={id}>
             <div className='project__wrapper'>
                 <h3 className='project__title'>{title}</h3>
-                <p className='project__description'>{description}</p>
+                {/*<p className='project__description'>{description}</p>*/}
+                <p className="project__description" dangerouslySetInnerHTML={createMarkup(description)}/>
                 <a href={link} target='_blank' rel='noreferrer'  className='project__link'>
                     <FontAwesomeIcon className='project__link-icon' icon={faGithub} />
                     Code
@@ -19,7 +25,7 @@ const Project = ({title, description, link, img, live}) => {
             </div>
             {/*SOMETHING IS WRONG HERE XDDD. This should be live but i dont why why i still get error "live is not defined"*/}
             <a href={live} target='_blank' rel='noreferrer' className='project__img-wrapper project__img-wrapper--active'>
-                <img src={img} alt={title} className='project__img' />
+                <img src={image.url} alt={title} className='project__img' />
                 <FontAwesomeIcon className='project__glass' icon={faSearch} />
             </a>
         </li>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,64 +4,7 @@ import gsap from 'gsap';
 import Project from './Project/Project';
 //styles
 import './Projects.scss';
-//TODO
-// USE some kind of graphcms/datocms to fetch the data;
-const projectsData = [
-  {
-    title: 'Github Jobs1',
-    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, cumque deserunt dicta expedita
-    inventore qui rerum temporibus.At aut, autem deserunt dolor doloremque eius harum ipsam laborum repellat sed
-    veniam.`,
-    img: 'https://i.imgur.com/dcXF5MS.png',
-    link: 'https://github.com/DonquixoteSabo/Github-Jobs',
-    live: 'https://donquixotesabo.github.io/Weather-App/',
-  },
-  {
-    title: 'Github Jobs2',
-    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, cumque deserunt dicta expedita
-    inventore qui rerum temporibus.At aut, autem deserunt dolor doloremque eius harum ipsam laborum repellat sed
-    veniam.`,
-    img: 'https://i.imgur.com/dcXF5MS.png',
-    link: 'https://github.com/DonquixoteSabo/Github-Jobs',
-    live: 'https://donquixotesabo.github.io/Weather-App/',
-  },
-  {
-    title: 'Github Jobs3',
-    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, cumque deserunt dicta expedita
-    inventore qui rerum temporibus.At aut, autem deserunt dolor doloremque eius harum ipsam laborum repellat sed
-    veniam.`,
-    img: 'https://i.imgur.com/dcXF5MS.png',
-    link: 'https://github.com/DonquixoteSabo/Github-Jobs',
-    live: 'https://donquixotesabo.github.io/Weather-App/',
-  },
-  {
-    title: 'Github Jobs4',
-    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, cumque deserunt dicta expedita
-    inventore qui rerum temporibus.At aut, autem deserunt dolor doloremque eius harum ipsam laborum repellat sed
-    veniam.`,
-    img: 'https://i.imgur.com/dcXF5MS.png',
-    link: 'https://github.com/DonquixoteSabo/Github-Jobs',
-    live: 'https://donquixotesabo.github.io/Weather-App/',
-  },
-  {
-    title: 'Github Jobs5',
-    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, cumque deserunt dicta expedita
-    inventore qui rerum temporibus.At aut, autem deserunt dolor doloremque eius harum ipsam laborum repellat sed
-    veniam.`,
-    img: 'https://i.imgur.com/dcXF5MS.png',
-    link: 'https://github.com/DonquixoteSabo/Github-Jobs',
-    live: 'https://donquixotesabo.github.io/Weather-App/',
-  },
-  {
-    title: 'Github Jobs6',
-    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, cumque deserunt dicta expedita
-    inventore qui rerum temporibus.At aut, autem deserunt dolor doloremque eius harum ipsam laborum repellat sed
-    veniam.`,
-    img: 'https://i.imgur.com/dcXF5MS.png',
-    link: 'https://github.com/DonquixoteSabo/Github-Jobs',
-    live: 'https://donquixotesabo.github.io/Weather-App/',
-  }
-];
+import projectsData from './ProjectData.json';
 
 const Projects = () => {
   const title = useRef(null);
