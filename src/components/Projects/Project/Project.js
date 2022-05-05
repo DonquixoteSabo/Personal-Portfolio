94ae8ec92a2624c7689c77fd9a0beaf9af262564
Kamil-Budzik <67870528+Kamil-Budzik@users.noreply.github.com>
2022-05-05 19:46:52 +0200

    Update Project

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -6,22 +6,16 @@ import {faSearch} from '@fortawesome/free-solid-svg-icons';
 //styles
 import './Project.scss';
 
-function createMarkup(description) {
-    return {__html: description};
-}
-// TODO
-// after you get images from your graphic designer refactor these styles because they don't look that good with these images.
 
 const Project = ({title, description, link, image, live, id}) => {
     return (
         <li className='project' key={id}>
             <div className='project__wrapper'>
                 <h3 className='project__title'>{title}</h3>
-                {/*<p className='project__description'>{description}</p>*/}
-                <p className="project__description" dangerouslySetInnerHTML={createMarkup(description)}/>
+                <p className='project__description'>{description}</p>
                 <a href={link} target='_blank' rel='noreferrer'  className='project__link'>
                     <FontAwesomeIcon className='project__link-icon' icon={faGithub} />
-                    Code
+                    Code 
                 </a>
             </div>
             {/*SOMETHING IS WRONG HERE XDDD. This should be live but i dont why why i still get error "live is not defined"*/}
