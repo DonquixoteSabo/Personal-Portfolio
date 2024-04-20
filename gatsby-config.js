d3bcba851cbba06d0208ecc39ced41e46bdf4ce8
DonquixoteSabo <mixboksu@gmail.com>
2024-04-20 20:08:03 +0200

    Netlify deploy fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: `Personal Porfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    'gatsby-plugin-netlify',
     'gatsby-plugin-sass',
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: 'src/images/favicon-32x32.png'
-      }
+        icon: 'src/images/favicon-32x32.png',
+      },
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
@@ -19,9 +20,10 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: './src/images/'
+        path: './src/images/',
       },
-      __key: 'images'
-    }, `gatsby-plugin-fontawesome-css`,
-  ]
+      __key: 'images',
+    },
+    `gatsby-plugin-fontawesome-css`,
+  ],
 };
